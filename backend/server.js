3e5a0609d084a4a3372883b37f9996181826add9
Harpal Singh <namrtanemo@gmail.com>
2025-09-29 15:19:13 +0530

    Fix Render deployment: uploads folder & start script

--- backend/server.js
+++ backend/server.js
@@ -5,14 +5,28 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import router from './MVC/routes/router.js';
 
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 dotenv.config();
 
 const app = express();
+
+const uploadsPath = path.join(__dirname, "uploads");
+if (!fs.existsSync(uploadsPath)) {
+  fs.mkdirSync(uploadsPath);
+}
 
 app.use(bodyParser.json());
 app.use(cors());
 app.use("/uploads", express.static("uploads"));
 app.use("/api", router);  // <-- router should be used before listening
+app.use("/uploads", express.static(uploadsPath));
+
 
 const PORT = process.env.PORT || 5000;
 const MONGOURL = process.env.MONGO_URL;
